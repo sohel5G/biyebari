2820f282219b3867c546417193e237f2d4c0a1e6
sohel5G <contactsohelrana@gmail.com>
2023-11-24 02:41:06 +0600

    basic frontend setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-center my-16 dark:text-blue-500">Router and tailwind css install done only</h1>
+    </>
+  )
+}
+
+export default App
