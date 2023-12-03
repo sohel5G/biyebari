6c35c6b6b5b7734ca8c6ba565c01b02e10667ad1
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-12-03 21:23:59 +0600

    url changed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://biyebari-server.vercel.app',
     withCredentials: true
 })
+
+// https://biyebari-server.vercel.app
+// http://localhost:5000
+
 
 const useAxiosSecure = () => {
 
